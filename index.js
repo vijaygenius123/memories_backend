f3a5ea69b9b83fc3b32304634f58de71cbed0ad8
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-11-08 22:37:35 +0530

    Updated Node And Tested App Listen

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+//import postRoutes from './routes/posts'
 
 const app = express();
+
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
@@ -13,8 +15,15 @@ app.use(cors());
 const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://localhost"
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then()
-    .catch();
+//mongoose.connect(MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+//    .then()
+//    .catch();
+
+app.get('/', ((req, res) => {
+    res.send('It Works')
+}))
 
 
+app.listen(PORT, () => {
+    console.log(`Server Running On Port:${PORT}`)
+})
