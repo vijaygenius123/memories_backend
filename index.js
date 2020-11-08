e692d0e4e7e7919efeb3d9c312244dcacdef628c
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-11-08 21:56:26 +0530

    DB Setup

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+
+const app = express();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://localhost"
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then()
+    .catch();
+
+
