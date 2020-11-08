42e25039009bb5c2b8ee884df81294ad42eee3bf
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-11-08 22:39:35 +0530

    Refactored Routes

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
-//import postRoutes from './routes/posts'
-
+import postRoutes from './routes/posts.js'
 const app = express();
 
-
+app.use('/posts', postRoutes);
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
@@ -18,11 +17,6 @@ const PORT = process.env.PORT || 5000;
 //mongoose.connect(MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
 //    .then()
 //    .catch();
-
-app.get('/', ((req, res) => {
-    res.send('It Works')
-}))
-
 
 app.listen(PORT, () => {
     console.log(`Server Running On Port:${PORT}`)
